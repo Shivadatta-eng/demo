bfb7559b8f98c49d1a509570e210dd5dbc759b6a
Shivadatta-eng <Shivadatta.shinde@tieto.com>
2026-06-10 13:39:03 +0530

    test pgm of pythons

---
+++ Test1.py
@@ -0,0 +1,74 @@
+# a = float(input("Enter a value of A: "))
+# b = float(input("Enter a value of B: "))
+# sum = a + b
+# print("The sum of A and B is", sum)
+
+
+# x =float(input("Enter a side of square :"))
+# area = x* x
+# print("The area of square is", area)
+
+
+# y = float(input("Enter first Number: "))             
+# z = float(input("Enter second Number: "))
+# u = (y +z) /2 
+# print("Averge of two numbers is : ", u)
+
+# a1 = int(input("Enter the first number:"))
+
+# a2 = int(input("Enter the second number:"))
+
+# if a1>= a2:
+#    print("True") 
+# else:
+#     print("False")
+# print(a1 >= a2)
+
+
+# str = "i am coder"
+# q  = str.endswith("ed")
+# print(q)
+      
+# str =str.capitalize()
+# print(str)
+
+# str = str.replace("er", "programmer")
+# print(str)
+
+# str = str.find("co")
+# print(str)
+
+# str2 = len(str)
+# print(str2)
+
+# str1 = input("Enter a string:")
+# # len1 = len(str1)
+# #print("length of string is:", len1)
+# count =str1.find("i")
+# print("The first occurrence of 'i' is at index:", count)
+
+# st1 = input("Enter the String ")
+# print(" your string will be ", st1)
+# c1 = st1.find("$")
+# print("postion of $ in your string at :", c1)
+
+# w1 = int (input ("Enter your number"))
+# if w1%2== 0:
+#     print("Your number is even number")
+# else:
+#    print("Your number is odd")
+
+d1 = int(input("Enter your number1 : "))
+d2 = int(input("Enter your number2 : "))
+d3 = int(input("Enter your number3 : "))
+
+if (d1 > d2) and  (d1> d3):
+    print (d1, "is grater number")
+
+elif (d2 > d1) and (d2 > d3):
+    print (d2, "is grater number")
+else:
+    print (d3, "is grater number")
+
+    
+
