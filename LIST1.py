2e78269deb383bb6b1f36b0526e50ffcff62cc61
Shivadatta-eng <Shivadatta.shinde@tieto.com>
2026-06-10 17:08:56 +0530

    new list pgms

--- LIST1.py
+++ LIST1.py
@@ -1,1 +1,67 @@
 # test pgm for lists
+# marks = [40, 60, 65, 55, 80, 75]
+# print(marks)
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+# print(marks[4])
+# print(marks[5])
+# print(marks[-1])  # to print reverse order / last element
+# print(marks[-2])  # to print second last element
+# print(marks[-3])  # to print third last element
+# print(marks[-4])  # to print fourth last element
+# print(marks[-5])  # to print fifth last element
+# print(marks[-6])  # to print sixth last element
+
+# Rollno = [101, 102, 103, 104, 105, 106]
+# print(Rollno[0:5]) # to print first five elements
+# print(Rollno[1:3]) # to print second and third element
+# print(Rollno[2:]) # to print from third element to last element
+# print(Rollno[:4]) # to print from first element to fourth element
+
+# ls1 =[21, 45, 90, 43, 33, 23]
+# ls1.append(56)
+# print(ls1)
+# ls1.sort() # to sort the list in ascending order
+# print(ls1)
+# ls1.sort(reverse=True) # to sort the list in descending order
+# print(ls1)
+# ls1.insert(4, 55) # to insert 55 at index 4
+# print(ls1)
+
+# tup = (12, 43, 61, 89, 65)
+# print(tup[0]) 
+
+
+# a1= [input("Please enter your three moive names:")]
+# print(a1)
+
+#palindrome
+
+# Palindrome check for a list
+# n = int(input("Enter the number of elements in the list: "))
+# b1 = []
+
+# for i in range(n):
+#     element = input(f"Enter element {i+1}: ")
+#     b1.append(element)
+
+# b2 = b1.copy()
+# b2.reverse()
+
+# if b1 == b2:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+tls1 = ["C", "D", "A", "A", "B", "B", "A"]
+# print(tup1.count("C"))
+
+tls1.sort()
+print(tls1)
+
+
+
+
+
