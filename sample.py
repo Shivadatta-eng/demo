9f0bde4d0bf2d9b34be52f15cc3d6a1c07019a53
Shivadatta-eng <Shivadatta.shinde@tieto.com>
2026-06-25 14:00:32 +0530

    new pgm

--- sample.py
+++ sample.py
@@ -28,11 +28,51 @@
 # for i in range(1, 11):
 #     print(i*i)
 
-i = 0
-tup1 = (1, 4, 9, 16, 25, 36, 49, 64, 81,100)
-e = (input("please enter a number:"))
-while(i<len(tup1)):
-     if(e==int(tup1[i])):
-            print("number is present in the tuple")
-            break
-i+=1
+# l1 = [1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+# len1 = len(l1)
+# i = 0
+# while(i<len1):
+#     print(l1[i])
+#     i+=1
+
+
+# for i in range(len(l1)):
+#     print(l1[i])
+               
+# l1= [1, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+# x = int(input("enter a number to search:"))
+# i=0
+# while(i<len(l1)):
+#     if l1[i] == x:
+#         print("found at index",i)
+#         break
+#     i+=1
+# else:
+#     print("not found")
+
+# z = int(input("enter a number: "))
+
+# sum = 0
+# for el in range(1, z+1):
+#     sum += el
+    
+# print("sum of numbers till ", z, "is", sum)
+# sum = 0
+# idx = 0
+# while(z>= idx):
+#     sum += idx
+#     idx +=1
+# print("sum of numbers till ", z, "is", sum)
+
+# for el in range(1, 5, 2):
+#     print(el)
+# for el in range(1, 5):
+#     print(el)
+# for el in range(5):
+#     print(el)
+
+# x = int(input("enter a number:"))
+
+l1 = [1, 5,9, 10, 18, 33,12, 14]
+print(len(l1))
+print(l1)
